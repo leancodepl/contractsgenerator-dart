d93f9ccdb1dcbbcdf24a21ff3231b4eb501e63e3
shilangyu <xmarcinmarcin@gmail.com>
2021-09-11 09:59:15 +0200

    Merge scripts

--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -4,3 +4,11 @@
 
 protoc -I=contractsgenerator --dart_out=lib/src/proto contractsgenerator/contracts.proto
 dart format lib/src/proto/*.dart
+
+
+echo "\
+part of 'generator_script.dart';
+
+const _\$script = r'''
+`cat contractsgenerator/tools/generate.sh`
+''';" > lib/src/generator_script.g.dart
--- scripts/generate_generator_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "\
-part of 'generator_script.dart';
-
-const _\$script = r'''
-`cat contractsgenerator/tools/generate.sh`
-''';" > lib/src/generator_script.g.dart
