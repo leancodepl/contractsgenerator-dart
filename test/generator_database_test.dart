c0478e6fe139c578b9daf7d93ce3562410a94623
shilangyu <xmarcinmarcin@gmail.com>
2021-12-23 22:52:52 +0100

    Add resolving generics

--- test/generator_database_test.dart
+++ test/generator_database_test.dart
@@ -21,7 +21,7 @@ GeneratorDatabase makeDb({
     );
 
 TypeRef typeRefOf(String name) =>
-    TypeRef(internal: TypeRef_Internal(name: name));
+    TypeRef(internal: TypeRef_Internal(name: name), nullable: true);
 
 void main() {
   group('generator database', () {
@@ -150,10 +150,13 @@ void main() {
 
     group('allPropertiesOf', () {
       test('gets one level deep', () {
-        final type = TypeRef(known: TypeRef_Known(type: KnownType.Command));
+        final type = TypeRef(
+          known: TypeRef_Known(type: KnownType.Command),
+          nullable: true,
+        );
         final properties = [
-          PropertyRef(name: 'prop1', type: type),
-          PropertyRef(name: 'prop2', type: type),
+          PropertyRef(name: 'prop1', type: type, comment: ''),
+          PropertyRef(name: 'prop2', type: type, comment: ''),
         ];
         final simple = Statement(
           name: 'simple',
@@ -170,10 +173,13 @@ void main() {
       });
 
       test('gets from linear inheritance', () {
-        final type = TypeRef(known: TypeRef_Known(type: KnownType.Command));
+        final type = TypeRef(
+          known: TypeRef_Known(type: KnownType.Command),
+          nullable: true,
+        );
         final properties1 = [
-          PropertyRef(name: 'prop1', type: type),
-          PropertyRef(name: 'prop2', type: type),
+          PropertyRef(name: 'prop1', type: type, comment: ''),
+          PropertyRef(name: 'prop2', type: type, comment: ''),
         ];
         final simple1 = Statement(
           name: 'simple1',
@@ -185,8 +191,8 @@ void main() {
           ),
         );
         final properties2 = [
-          PropertyRef(name: 'prop3', type: type),
-          PropertyRef(name: 'prop4', type: type),
+          PropertyRef(name: 'prop3', type: type, comment: ''),
+          PropertyRef(name: 'prop4', type: type, comment: ''),
         ];
         final simple2 = Statement(
           name: 'simple2',
@@ -198,8 +204,8 @@ void main() {
           ),
         );
         final properties3 = [
-          PropertyRef(name: 'prop5', type: type),
-          PropertyRef(name: 'prop6', type: type),
+          PropertyRef(name: 'prop5', type: type, comment: ''),
+          PropertyRef(name: 'prop6', type: type, comment: ''),
         ];
         final simple3 = Statement(
           name: 'simple3',
@@ -218,17 +224,20 @@ void main() {
 
         expect(
           db.allPropertiesOf(simple1),
-          properties1 + properties2 + properties3,
+          properties3 + properties2 + properties1,
         );
-        expect(db.allPropertiesOf(simple2), properties2 + properties3);
+        expect(db.allPropertiesOf(simple2), properties3 + properties2);
         expect(db.allPropertiesOf(simple3), properties3);
       });
 
       test('gets from multi inheritance', () {
-        final type = TypeRef(known: TypeRef_Known(type: KnownType.Command));
+        final type = TypeRef(
+          known: TypeRef_Known(type: KnownType.Command),
+          nullable: true,
+        );
         final properties1 = [
-          PropertyRef(name: 'prop1', type: type),
-          PropertyRef(name: 'prop2', type: type),
+          PropertyRef(name: 'prop1', type: type, comment: ''),
+          PropertyRef(name: 'prop2', type: type, comment: ''),
         ];
         final simple1 = Statement(
           name: 'simple1',
@@ -240,8 +249,8 @@ void main() {
           ),
         );
         final properties2 = [
-          PropertyRef(name: 'prop3', type: type),
-          PropertyRef(name: 'prop4', type: type),
+          PropertyRef(name: 'prop3', type: type, comment: ''),
+          PropertyRef(name: 'prop4', type: type, comment: ''),
         ];
         final simple2 = Statement(
           name: 'simple2',
@@ -250,8 +259,8 @@ void main() {
           ),
         );
         final properties3 = [
-          PropertyRef(name: 'prop5', type: type),
-          PropertyRef(name: 'prop6', type: type),
+          PropertyRef(name: 'prop5', type: type, comment: ''),
+          PropertyRef(name: 'prop6', type: type, comment: ''),
         ];
         final simple3 = Statement(
           name: 'simple3',
@@ -270,10 +279,279 @@ void main() {
 
         expect(
           db.allPropertiesOf(simple1),
-          properties1 + properties2 + properties3,
+          properties2 + properties3 + properties1,
         );
         expect(db.allPropertiesOf(simple2), properties2);
         expect(db.allPropertiesOf(simple3), properties3);
+      });
+
+      test('resolves generics', () {
+        final a = Statement(
+          name: 'A',
+          dto: Statement_DTO(
+            typeDescriptor: TypeDescriptor(
+              genericParameters: [GenericParameter(name: 'T')],
+              properties: [
+                PropertyRef(
+                  name: 'a',
+                  type: TypeRef(
+                    nullable: true,
+                    known: TypeRef_Known(
+                      type: KnownType.Map,
+                      arguments: [
+                        TypeRef(
+                          nullable: true,
+                          generic: TypeRef_Generic(name: 'T'),
+                        ),
+                        TypeRef(
+                          nullable: true,
+                          known: TypeRef_Known(
+                            type: KnownType.Array,
+                            arguments: [
+                              TypeRef(
+                                nullable: true,
+                                generic: TypeRef_Generic(name: 'T'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  comment: '',
+                ),
+                PropertyRef(
+                  name: 'b',
+                  type: TypeRef(
+                    nullable: true,
+                    generic: TypeRef_Generic(name: 'T'),
+                  ),
+                  comment: '',
+                ),
+              ],
+            ),
+          ),
+        );
+        final b = Statement(
+          name: 'B',
+          dto: Statement_DTO(
+            typeDescriptor: TypeDescriptor(
+              genericParameters: [GenericParameter(name: 'T')],
+              extends_1: [
+                TypeRef(
+                  internal: TypeRef_Internal(
+                    name: 'A',
+                    arguments: [
+                      TypeRef(
+                        nullable: true,
+                        known: TypeRef_Known(
+                          type: KnownType.Array,
+                          arguments: [
+                            TypeRef(
+                              nullable: true,
+                              generic: TypeRef_Generic(name: 'T'),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+              properties: [
+                PropertyRef(
+                  name: 'b',
+                  type: TypeRef(
+                    nullable: true,
+                    known: TypeRef_Known(
+                      type: KnownType.Array,
+                      arguments: [
+                        TypeRef(
+                          nullable: true,
+                          generic: TypeRef_Generic(name: 'T'),
+                        )
+                      ],
+                    ),
+                  ),
+                  comment: '',
+                ),
+              ],
+            ),
+          ),
+        );
+        final c = Statement(
+          name: 'C',
+          dto: Statement_DTO(
+            typeDescriptor: TypeDescriptor(
+              genericParameters: [
+                GenericParameter(name: 'T'),
+                GenericParameter(name: 'U'),
+              ],
+              properties: [
+                PropertyRef(
+                  name: 'c',
+                  type: TypeRef(
+                    nullable: true,
+                    generic: TypeRef_Generic(name: 'T'),
+                  ),
+                  comment: '',
+                ),
+                PropertyRef(
+                  name: 'd',
+                  type: TypeRef(
+                    nullable: true,
+                    generic: TypeRef_Generic(name: 'U'),
+                  ),
+                  comment: '',
+                ),
+              ],
+            ),
+          ),
+        );
+        final dto = Statement(
+          name: 'DTO',
+          dto: Statement_DTO(
+            typeDescriptor: TypeDescriptor(
+              genericParameters: [GenericParameter(name: 'T')],
+              extends_1: [
+                TypeRef(
+                  nullable: true,
+                  internal: TypeRef_Internal(
+                    name: 'B',
+                    arguments: [
+                      TypeRef(
+                        nullable: true,
+                        known: TypeRef_Known(type: KnownType.Float64),
+                      ),
+                    ],
+                  ),
+                ),
+                TypeRef(
+                  nullable: true,
+                  internal: TypeRef_Internal(
+                    name: 'C',
+                    arguments: [
+                      TypeRef(
+                        nullable: true,
+                        generic: TypeRef_Generic(name: 'T'),
+                      ),
+                      TypeRef(
+                        nullable: true,
+                        known: TypeRef_Known(type: KnownType.String),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+              properties: [
+                PropertyRef(
+                  name: 'd',
+                  type: TypeRef(
+                    nullable: true,
+                    known: TypeRef_Known(type: KnownType.String),
+                  ),
+                  comment: '',
+                ),
+                PropertyRef(
+                  name: 'e',
+                  type: TypeRef(
+                    nullable: true,
+                    generic: TypeRef_Generic(name: 'T'),
+                  ),
+                  comment: '',
+                ),
+              ],
+            ),
+          ),
+        );
+
+        final db = makeDb(rawStatements: [a, b, c, dto]);
+
+        expect(
+          db.allPropertiesOf(dto),
+          [
+            PropertyRef(
+              name: 'a',
+              type: TypeRef(
+                nullable: true,
+                known: TypeRef_Known(
+                  type: KnownType.Map,
+                  arguments: [
+                    TypeRef(
+                      nullable: true,
+                      known: TypeRef_Known(
+                        type: KnownType.Array,
+                        arguments: [
+                          TypeRef(
+                            nullable: true,
+                            known: TypeRef_Known(type: KnownType.Float64),
+                          )
+                        ],
+                      ),
+                    ),
+                    TypeRef(
+                      nullable: true,
+                      known: TypeRef_Known(
+                        type: KnownType.Array,
+                        arguments: [
+                          TypeRef(
+                            nullable: true,
+                            known: TypeRef_Known(
+                              type: KnownType.Array,
+                              arguments: [
+                                TypeRef(
+                                  nullable: true,
+                                  known: TypeRef_Known(type: KnownType.Float64),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              comment: '',
+            ),
+            PropertyRef(
+              name: 'b',
+              type: TypeRef(
+                nullable: true,
+                known: TypeRef_Known(
+                  type: KnownType.Array,
+                  arguments: [
+                    TypeRef(
+                      nullable: true,
+                      known: TypeRef_Known(type: KnownType.Float64),
+                    )
+                  ],
+                ),
+              ),
+              comment: '',
+            ),
+            PropertyRef(
+              name: 'c',
+              type:
+                  TypeRef(nullable: false, generic: TypeRef_Generic(name: 'T')),
+              comment: '',
+            ),
+            PropertyRef(
+              name: 'd',
+              type: TypeRef(
+                nullable: true,
+                known: TypeRef_Known(type: KnownType.String),
+              ),
+              comment: '',
+            ),
+            PropertyRef(
+              name: 'e',
+              type:
+                  TypeRef(nullable: false, generic: TypeRef_Generic(name: 'T')),
+              comment: '',
+            ),
+          ],
+        );
       });
     });
   });
