11b3e8e691ec235cd0a17d11edea23b44f736955
shilangyu <xmarcinmarcin@gmail.com>
2022-07-22 16:01:54 +0200

    Add ContractsSerializable

--- packages/leancode_contracts/lib/leancode_contracts.dart
+++ packages/leancode_contracts/lib/leancode_contracts.dart
@@ -2,6 +2,7 @@ export 'package:cqrs/cqrs.dart';
 export 'package:equatable/equatable.dart';
 export 'package:json_annotation/json_annotation.dart';
 
+export 'src/contracts_serializable.dart';
 export 'src/json_converters/duration_json_converter.dart';
 export 'src/types/date_only.dart';
 export 'src/types/date_time_offset.dart';
