383b0c9798f4e9519f1b2f37018567935acef2a7
Piotr Rogulski <piter.rogulski@gmail.com>
2025-08-19 09:53:39 +0200

    Reformat example

--- packages/leancode_contracts_generator/example/lib/cool_name.g.dart
+++ packages/leancode_contracts_generator/example/lib/cool_name.g.dart
@@ -22,81 +22,74 @@ Map<String, dynamic> _$RolesToJson(Roles instance) => <String, dynamic>{};
 PaginatedResult<TResult> _$PaginatedResultFromJson<TResult>(
   Map<String, dynamic> json,
   TResult Function(Object? json) fromJsonTResult,
-) =>
-    PaginatedResult<TResult>(
-      items: (json['Items'] as List<dynamic>).map(fromJsonTResult).toList(),
-      totalCount: json['TotalCount'] as int,
-    );
+) => PaginatedResult<TResult>(
+  items: (json['Items'] as List<dynamic>).map(fromJsonTResult).toList(),
+  totalCount: json['TotalCount'] as int,
+);
 
 Map<String, dynamic> _$PaginatedResultToJson<TResult>(
   PaginatedResult<TResult> instance,
   Object? Function(TResult value) toJsonTResult,
-) =>
-    <String, dynamic>{
-      'Items': instance.items.map(toJsonTResult).toList(),
-      'TotalCount': instance.totalCount,
-    };
+) => <String, dynamic>{
+  'Items': instance.items.map(toJsonTResult).toList(),
+  'TotalCount': instance.totalCount,
+};
 
 ISomethingRelated _$ISomethingRelatedFromJson(Map<String, dynamic> json) =>
-    ISomethingRelated(
-      somethingId: json['SomethingId'] as String,
-    );
+    ISomethingRelated(somethingId: json['SomethingId'] as String);
 
 Map<String, dynamic> _$ISomethingRelatedToJson(ISomethingRelated instance) =>
-    <String, dynamic>{
-      'SomethingId': instance.somethingId,
-    };
+    <String, dynamic>{'SomethingId': instance.somethingId};
 
 WhenHasSomethingAccess _$WhenHasSomethingAccessFromJson(
-        Map<String, dynamic> json) =>
-    WhenHasSomethingAccess();
+  Map<String, dynamic> json,
+) => WhenHasSomethingAccess();
 
 Map<String, dynamic> _$WhenHasSomethingAccessToJson(
-        WhenHasSomethingAccess instance) =>
-    <String, dynamic>{};
+  WhenHasSomethingAccess instance,
+) => <String, dynamic>{};
 
 AllUsers _$AllUsersFromJson(Map<String, dynamic> json) => AllUsers(
-      pageNumber: json['PageNumber'] as int,
-      pageSize: json['PageSize'] as int,
-    );
+  pageNumber: json['PageNumber'] as int,
+  pageSize: json['PageSize'] as int,
+);
 
 Map<String, dynamic> _$AllUsersToJson(AllUsers instance) => <String, dynamic>{
-      'PageNumber': instance.pageNumber,
-      'PageSize': instance.pageSize,
-    };
+  'PageNumber': instance.pageNumber,
+  'PageSize': instance.pageSize,
+};
 
 EditUser _$EditUserFromJson(Map<String, dynamic> json) => EditUser(
-      somethingId: json['SomethingId'] as String,
-      userId: json['UserId'] as String,
-      list: (json['List'] as List<dynamic>).map((e) => e as int).toList(),
-      array: (json['Array'] as List<dynamic>).map((e) => e as int).toList(),
-      dictionary: (json['Dictionary'] as Map<String, dynamic>).map(
-        (k, e) => MapEntry(
-            int.parse(k), EditUser.fromJson(e as Map<String, dynamic>)),
-      ),
-      userInfo: UserInfoDTO.fromJson(json['UserInfo'] as Map<String, dynamic>),
-    );
+  somethingId: json['SomethingId'] as String,
+  userId: json['UserId'] as String,
+  list: (json['List'] as List<dynamic>).map((e) => e as int).toList(),
+  array: (json['Array'] as List<dynamic>).map((e) => e as int).toList(),
+  dictionary: (json['Dictionary'] as Map<String, dynamic>).map(
+    (k, e) =>
+        MapEntry(int.parse(k), EditUser.fromJson(e as Map<String, dynamic>)),
+  ),
+  userInfo: UserInfoDTO.fromJson(json['UserInfo'] as Map<String, dynamic>),
+);
 
 Map<String, dynamic> _$EditUserToJson(EditUser instance) => <String, dynamic>{
-      'SomethingId': instance.somethingId,
-      'UserId': instance.userId,
-      'List': instance.list,
-      'Array': instance.array,
-      'Dictionary':
-          instance.dictionary.map((k, e) => MapEntry(k.toString(), e)),
-      'UserInfo': instance.userInfo,
-    };
+  'SomethingId': instance.somethingId,
+  'UserId': instance.userId,
+  'List': instance.list,
+  'Array': instance.array,
+  'Dictionary': instance.dictionary.map((k, e) => MapEntry(k.toString(), e)),
+  'UserInfo': instance.userInfo,
+};
 
 UserById _$UserByIdFromJson(Map<String, dynamic> json) => UserById();
 
 Map<String, dynamic> _$UserByIdToJson(UserById instance) => <String, dynamic>{};
 
 UserInfoDTO _$UserInfoDTOFromJson(Map<String, dynamic> json) => UserInfoDTO(
-      firstname: json['Firstname'] as String,
-      surname: json['Surname'] as String,
-      username: json['Username'] as String,
-      emailAddress: json['EmailAddress'] as String,
-    );
+  firstname: json['Firstname'] as String,
+  surname: json['Surname'] as String,
+  username: json['Username'] as String,
+  emailAddress: json['EmailAddress'] as String,
+);
 
 Map<String, dynamic> _$UserInfoDTOToJson(UserInfoDTO instance) =>
     <String, dynamic>{
